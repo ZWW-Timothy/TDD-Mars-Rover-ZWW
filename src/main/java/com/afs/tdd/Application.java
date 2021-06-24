3b94d3bee8f75f7f374698a33797ac6876350885
ZWW-Timothy <zhaowuwei2020@foxmail.com>
2021-06-25 01:21:34 +0800

    [Wuwei Zhao] feat: complete application.

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,4 +1,12 @@
 package com.afs.tdd;
 
 public class Application {
+
+    public static void main(String[] args) {
+        Rover rover = new Rover();
+        rover.init(1, -1, "E");
+        Rover roverResult = rover.executeInstruction("MLMMRMMLLMRRML");
+        System.out.println("(" + roverResult.getPosition().getX() + ", " + roverResult.getPosition().getY() + ")");
+        System.out.println(rover.getDirection().toString());
+    }
 }
