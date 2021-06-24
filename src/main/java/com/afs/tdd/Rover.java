0a7fb11e2feeab2cccfb800de889e78ff4cea010
ZWW-Timothy <zhaowuwei2020@foxmail.com>
2021-06-24 15:32:12 +0800

    [Wuwei Zhao] feat: complete init method of Rover.

--- src/main/java/com/afs/tdd/Rover.java
+++ src/main/java/com/afs/tdd/Rover.java
@@ -2,18 +2,34 @@ package com.afs.tdd;
 
 public class Rover {
 
+    private Integer xPosition;
+    private Integer yPosition;
+    private String direction;
+
+    public Rover() {
+    }
+
+    public Rover(Integer xPosition, Integer yPosition, String direction) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.direction = direction;
+    }
+
     public void init(int x, int y, String direction) {
+        this.xPosition = x;
+        this.yPosition = y;
+        this.direction = direction;
     }
 
     public int getXPosition() {
-        return 1;
+        return this.xPosition;
     }
 
     public int getYPosition() {
-        return 1;
+        return this.yPosition;
     }
 
     public String getDirection() {
-        return "S";
+        return this.direction;
     }
 }
